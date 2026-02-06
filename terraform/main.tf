f9182411f3845eedef17a1d352544ff427b8bbf0
Jothika S <jothijoo2207@gmail.com>
2026-02-06 11:30:00 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,7 +87,7 @@ resource "aws_db_instance" "mysql" {
  
 resource "aws_instance" "web" {
   count = 2
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  ami           = "ami-0b6c6ebed2801a5cb" # Amazon Linux 2
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public[count.index].id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
